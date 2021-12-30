48740a9d01e01b67195567383d8ea1449dbd3cfa
JeongSubi <jsb97613@naver.com>
2021-12-30 21:47:00 +0900

    feat: login

--- server/API/controller/user.ts
+++ server/API/controller/user.ts
@@ -1,6 +1,59 @@
-import { Request, Response } from 'express';
-interface UserController {}
+import { Request, Response } from "express";
+import { CustomRequest } from "../../type/type";
 
-const userController: UserController = {};
+interface UserController {
+  login(req: CustomRequest, res: Response): Promise<void>;
+  logout(req: CustomRequest, res: Response): Promise<void>;
+  signup(req: CustomRequest, res: Response): Promise<void>;
+  signout(req: CustomRequest, res: Response): Promise<void>;
+}
+
+const userController: UserController = {
+  /* 
+  로그인
+  */
+  async login(req: CustomRequest, res: Response): Promise<void> {
+    // 작성
+    // req.sendData={}
+    if (req.sendData.message === "no exists email") {
+      res.status(400).send({ message: "no exists email" });
+    } else if (req.sendData.message === "incorrect password") {
+      res.status(401).send({ message: "incorrect password" });
+    } else if (req.sendData.message === "ok") {
+      res.cookie("refreshToken", req.sendData.data.refreshToken, {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+      });
+      res.status(200).send({
+        data: { accessToken: req.sendData.data.accessToken },
+        message: "ok",
+      });
+    } else if (req.sendData.message === "err") {
+      res.status(500).send({ message: "err" });
+    }
+  },
+
+  /*
+  로그아웃
+  */
+  async logout(req: CustomRequest, res: Response): Promise<void> {
+    if (req.sendData.message === "ok") {
+      // 클라이언트에서 aceessToken 지워주세요
+      res.clearCookie("refreshToken");
+      res.status(200).send({ message: "ok" });
+    }
+  },
+
+  /*
+  회원가입
+  */
+  async signup(req: CustomRequest, res: Response): Promise<void> {},
+
+  /*
+  회원탈퇴
+  */
+  async signout(req: CustomRequest, res: Response): Promise<void> {},
+};
 
 export default userController;
