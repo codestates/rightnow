075716cba7ce6a4bba5fb0a5893cbca4dab6603c
JeongSubi <jsb97613@naver.com>
2021-12-31 10:32:41 +0900

    feat:signup

--- server/API/validation/accessTokenRequest.ts
+++ server/API/validation/accessTokenRequest.ts
@@ -1,62 +1,55 @@
-import { Request, Response, NextFunction } from "express";
-import { CustomRequest } from "../../type/type";
+import { Request, Response, NextFunction } from 'express';
+import { CustomRequest } from '../../type/type';
 
-const dotenv: any = require("dotenv");
+const dotenv: any = require('dotenv');
 dotenv.config();
 
-const db: any = require("../../models/index");
-const jwt: any = require("jsonwebtoken");
+const db: any = require('../../models/index');
+const jwt: any = require('jsonwebtoken');
 
 interface AccessTokenRequestController {
   accessTokenRequest(
     req: CustomRequest,
     res: Response,
-    type: string
+    type: string,
   ): Promise<any>;
 }
 
 const accessTokenRequestController: AccessTokenRequestController = {
   async accessTokenRequest(req: CustomRequest, res: Response, type: string) {
-    //req.sendData = 'send';
-
-    const refreshToken = req.cookies.refreshToken;
+    const refreshToken: any = req.cookies.refreshToken;
     if (!refreshToken) {
-      res.status(403).json({ message: "refresh token not provided" });
+      res.status(403).json({ message: 'refresh token not provided' });
     } else {
       jwt.verify(
         refreshToken,
         process.env.REFRESH_SECRET,
-        async (err, decoded) => {
+        async (err: any, decoded: any) => {
           if (err) {
             res.status(400).json({
-              message: "invalid refresh token, please log in again",
+              message: 'invalid refresh token, please log in again',
             });
           } else {
-            const userInfo = await db["User"].findOne({
+            const userInfo = await db['User'].findOne({
               where: { email: decoded.email },
             });
             delete userInfo.dataValues.password;
 
             if (!userInfo) {
               res.status(404).json({
-                message: "refresh token has been tempered",
+                message: 'token has been tempered',
               });
             } else {
               const accessToken = jwt.sign(
                 userInfo.dataValues,
                 process.env.ACCESS_SECRET,
                 {
-                  expiresIn: "15m",
-                }
+                  expiresIn: '15m',
+                },
               );
-              if (type === "update") {
-                const { email } = req.params;
-                const { name, nickname } = req.body;
-                await db["User"].update(
-                  { name, nickname },
-                  { where: { email } }
-                );
-                userInfo.dataValues.name = name;
+              if (type === 'update') {
+                const { email, nickname } = req.body;
+                await db['User'].update({ nickname }, { where: { email } });
                 userInfo.dataValues.nickname = nickname;
               }
 
@@ -65,11 +58,11 @@ const accessTokenRequestController: AccessTokenRequestController = {
                   userInfo: userInfo.dataValues,
                   accessToken: accessToken,
                 },
-                message: "ok",
+                message: 'ok',
               });
             }
           }
-        }
+        },
       );
     }
   },
