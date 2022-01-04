a3b085bc8efdabb71faa5ea7abd14c8684ab4fc1
JeongSubi <jsb97613@naver.com>
2022-01-04 10:37:39 +0900

    feat:admin

--- server/API/controller/admin.ts
+++ server/API/controller/admin.ts
@@ -1,14 +1,63 @@
 import { Request, Response } from 'express';
 import { CustomRequest } from '../../type/type';
+
+const dotenv: any = require('dotenv');
+dotenv.config();
+
 interface AdminController {
   getReportedUser(req: CustomRequest, res: Response): Promise<void>;
+  giveAuthority(req: CustomRequest, res: Response): Promise<void>;
+  takeAuthority(req: CustomRequest, res: Response): Promise<void>;
 }
 
 const adminController: AdminController = {
-  async getReportedUser(req: CustomRequest, res: Response): Promise<void> {
-    // 작성
-    //res.send(req.sendData);
+  /*
+  신고된 유저와 메시지 보기
+  */
+  async getReportedUser(req: CustomRequest, res: Response): Promise<void> {},
+
+  /*
+  관리자 권한 부여
+  */
+  async giveAuthority(req: CustomRequest, res: Response): Promise<void> {
+    if (req.sendData.message === 'not root account') {
+      res.status(404).send({ message: 'not root account' });
+    } else if (req.sendData.message === 'ok') {
+      res.status(200).send({
+        data: { userInfo: req.sendData.data.userInfo },
+        message: 'ok',
+      });
+    } else if (req.sendData.message === 'no exists user account') {
+      res.status(409).send({ message: 'no exists user account' });
+    } else if (
+      req.sendData.message === 'refreshToken not provided' ||
+      req.sendData.message === 'invalid refresh token' ||
+      req.sendData.message === 'token has been tempered'
+    ) {
+      res
+        .status(400)
+        .json({ message: 'invalid refreshToken, please log in again' });
+    } else if (req.sendData.message === 'token has been tempered') {
+      res.status(403).json({
+        message: 'token has been tempered',
+      });
+    } else if (
+      req.sendData.message === 'ok, give new accessToken and refreshToken'
+    ) {
+      res.status(200).json({
+        data: {
+          userInfo: req.sendData.data.userInfo,
+          accessToken: req.sendData.data.accessToken,
+        },
+        message: 'ok',
+      });
+    }
   },
+
+  /* 
+  관리자 권한 뺏기
+  */
+  async takeAuthority(req: CustomRequest, res: Response): Promise<void> {},
 };
 
 export default adminController;
--- server/API/validation/accessTokenRequest.ts
+++ server/API/validation/accessTokenRequest.ts
@@ -38,36 +38,76 @@ const accessTokenRequestValidation: AccessTokenRequestValidation = {
             };
             next();
           } else {
+            const { email } = req.body;
             const userInfo: any = await db['User'].findOne({
               where: { email: decoded.email },
             });
             delete userInfo.dataValues.password;
-            if (!userInfo) {
-              req.sendData = {
-                message: 'token has been tempered',
-              };
-              next();
-            } else {
-              const accessToken: any = jwt.sign(
-                userInfo.dataValues,
-                process.env.ACCESS_SECRET,
-                {
-                  expiresIn: '15m',
-                },
-              );
-              if (type === 'update') {
-                const { email, nick_name } = req.body;
-                await db['User'].update({ nick_name }, { where: { email } });
-                userInfo.dataValues.nick_name = nick_name;
+
+            if (type === 'admin') {
+              const adminInfo: any = await db['User'].findOne({
+                where: { email: decoded.email },
+              });
+              delete adminInfo.dataValues.password;
+              if (!adminInfo) {
+                req.sendData = {
+                  message: 'token has been tempered',
+                };
+                next();
+              } else {
+                if (adminInfo.role === 'ROOT') {
+                  const accessToken: any = jwt.sign(
+                    adminInfo.dataValues,
+                    process.env.ACCESS_SECRET,
+                    {
+                      expiresIn: '15m',
+                    },
+                  );
+                  await db['User'].update(
+                    { role: 'ADMIN' },
+                    { where: { email } },
+                  );
+                  userInfo.dataValues.role = 'ADMIN';
+                  req.sendData = {
+                    data: {
+                      userInfo: userInfo.dataValues,
+                      accessToken: accessToken,
+                    },
+                    message: 'ok, give new accessToken and refreshToken',
+                  };
+                  next();
+                } else {
+                  req.sendData = { message: 'not root account' };
+                }
               }
-              req.sendData = {
-                data: {
-                  userInfo: userInfo.dataValues,
-                  accessToken: accessToken,
-                },
-                message: 'ok, give new accessToken and refreshToken',
-              };
-              next();
+            } else {
+              if (!userInfo) {
+                req.sendData = {
+                  message: 'token has been tempered',
+                };
+                next();
+              } else {
+                const accessToken: any = jwt.sign(
+                  userInfo.dataValues,
+                  process.env.ACCESS_SECRET,
+                  {
+                    expiresIn: '15m',
+                  },
+                );
+                if (type === 'update') {
+                  const { email, nick_name } = req.body;
+                  await db['User'].update({ nick_name }, { where: { email } });
+                  userInfo.dataValues.nick_name = nick_name;
+                }
+                req.sendData = {
+                  data: {
+                    userInfo: userInfo.dataValues,
+                    accessToken: accessToken,
+                  },
+                  message: 'ok, give new accessToken and refreshToken',
+                };
+                next();
+              }
             }
           }
         },
