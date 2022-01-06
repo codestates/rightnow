dbf539a5bc0d3bb0e052d9abd9d120869a8bb308
kimdoyeonn <doyeon311@gmail.com>
2022-01-06 16:09:27 +0900

    Feat: 로딩 스피너적용

--- client/src/components/Loading.tsx
+++ client/src/components/Loading.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Folding, BounceBall } from './Spinner';
 
 const Container = styled.div`
   position: absolute;
@@ -7,23 +8,28 @@ const Container = styled.div`
   left: 0;
   width: 100%;
   height: 100%;
-  background-color: rgb(255, 255, 255, 0.3);
+  background-color: rgb(0, 0, 0, 0.4);
   z-index: 10;
   transition: 0.3s;
 `;
 
-const Content = styled.div`
+const LoadingContainer = styled.div`
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translateX(-50%) translateY(-50%);
+  /* margin: 20px auto; */
 `;
 
 const Loading = () => {
   return (
-    <Container>
-      <Content>Loading</Content>
-    </Container>
+    <>
+      <Container>
+        <LoadingContainer>
+          <BounceBall />
+        </LoadingContainer>
+      </Container>
+    </>
   );
 };
 
