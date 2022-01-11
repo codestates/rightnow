9ebf7ac6159965b01f62a04915c15f053d39067c
kimdoyeonn <doyeon311@gmail.com>
2022-01-11 16:14:21 +0900

    Refact: 중복 interface 코드정리

--- client/src/components/Chatting.tsx
+++ client/src/components/Chatting.tsx
@@ -10,6 +10,7 @@ import { useAppSelector } from '../config/hooks';
 import { userEmail } from '../reducers/userSlice';
 import Message from './Message';
 import ModalTemp from './ModalTemp';
+import { MessageType } from '../type';
 
 const ChattingContainer = styled.div`
   height: 100%;
@@ -209,15 +210,6 @@ interface ChattingProps {
   handleQuit: MouseEventHandler<HTMLButtonElement>;
   handleInsertMessage: FormEventHandler<HTMLFormElement>;
   updateMessage: any;
-}
-
-interface MessageType {
-  id: number;
-  User: { email: string; nick_name: string; profile_image: string }; // fix - profile_img -> profile_image
-  content: string;
-  is_update: string;
-  write_date: string;
-  isAlarm?: boolean; // fix - 채팅방 알람타입 인지 확인위해 (유저 입장, 퇴장 시)
 }
 
 const ChattingRoom = ({
