28bf0ca56201c6d1bf3991101b2974fe32b7a3db
kimdoyeonn <doyeon311@gmail.com>
2022-01-12 14:10:32 +0900

    Feat: Alert 컴포넌트 지움

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -219,7 +219,8 @@ const Room = () => {
       } = await roomAPI.getRoomInfo(room_id, email);
       setTalkContents(Messages);
       setRoomLocation(location);
-
+      const data = await roomAPI.getRoomInfo(room_id, email);
+      console.log(data);
       const members = Participants.map((member: any) => {
         return member.User;
       });
