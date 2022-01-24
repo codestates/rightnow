dfff428d50732dd07a9615bdec9e8e86db3106f5
namsupark <skatn7979@gmail.com>
2022-01-24 14:46:01 +0900

    Final Change

--- server/routers/socket.ts
+++ server/routers/socket.ts
@@ -18,67 +18,32 @@ import chatMethod from '../method/chat';
 // const db: any = require('../models/index');
 const socketRouter: Router = express.Router();
 const cors: any = require('cors');
-// socketRouter.use(function (req, res, next) {
-//   res.header(
-//     'Access-Control-Allow-Origin',
-//     'https://codebaker-rightnow.netlify.app',
-//   );
-//   res.header(
-//     'Access-Control-Allow-Headers',
-//     'Origin, X-Requested-With, Content-Type, Accept',
-//   );
-//   res.header('Access-Control-Allow-Credentials', 'true');
-//   next();
-// });
-socketRouter.use(
-  cors({
-    origin: 'https://codebaker-rightnow.netlify.app',
-    credentials: true,
-    methods: ['PATCH', 'POST', 'DELETE', 'GET', 'PUT', 'OPTIONS'],
-  }),
-);
+
 const httpServer: any = require('http');
-const http: Http2Server | any = httpServer.createServer(socketRouter);
+const http: Http2Server | any = httpServer.createServer();
 const socketPort: number = 4000;
-// const UUID_FUNC: Function = require('../method/uuid');
-// const SEARCH_COUNT: number = 5;
-// let { findUsers, tempRooms, roomList, attendUsers } = require('../data/cache');
 
-// http.listen(socketPort, () => {
-//   console.log('listening on *:' + socketPort);
-// });
-
-// const io: Server | any = require('socket.io')(http, {
-//   cors: {
-//     origin: 'https://codebaker-rightnow.netlify.app',
-//     methods: ['PATCH', 'POST', 'DELETE', 'GET', 'PUT', 'OPTIONS'],
-//     credentials: true,
-//   },
-// });
 const io = new Server(http, {
   cors: {
-    origin: 'https://codebaker-rightnow.netlify.app',
+    origin: 'https://right-now.link',
     credentials: true,
     methods: ['PATCH', 'POST', 'DELETE', 'GET', 'PUT', 'OPTIONS'],
   },
 });
-// , {
-//   cors: {
-//     origin: 'https://codebaker-rightnow.netlify.app',
-//     credentials: true,
-//   },
-// }
+
 http.listen(socketPort, () => {
   console.log('listening on *:' + socketPort);
 });
 //client 에서  주소/search 로 보내줘야 함
-
 const searchNamespace: Namespace = io.of('/search');
 
 const chatNamespace: Namespace = io.of('/chat');
+try {
+  searchNamespace.on('connection', searchMethod);
 
-searchNamespace.on('connection', searchMethod);
-
-chatNamespace.on('connection', chatMethod);
+  chatNamespace.on('connection', chatMethod);
+} catch (e) {
+  console.log(e);
+}
 export default socketRouter;
 export { chatNamespace, searchNamespace };
